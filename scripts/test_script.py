8e5a4c05fb4a6cec37934a8937f650dc8ac7b3c6
Scott Gigante <scottgigante@gmail.com>
2018-06-27 11:20:58 -0400

    simple test 10X upload

--- scripts/test_script.py
+++ scripts/test_script.py
@@ -1,4 +1,9 @@
 import sys
+from phate_io import run_phate
 
+run_phate("../spoof_uploads/test_10X",
+          sparse=None,
+          gene_labels='symbol',
+          allow_duplicates=False)
 sys.stdout.write("OUT DATA TEST SUCESSFUL")
 exit(1)
