cb8823bd7be7cd3b82e097feb38688b4e6f3c86f
Scott Gigante <scottgigante@gmail.com>
2018-07-11 21:22:55 -0400

    test phate again

--- scripts/test_script.py
+++ scripts/test_script.py
@@ -1,17 +1,13 @@
 import sys
-# from phate_io import run_phate
-#
-# run_phate("test_10X.zip",
-#           sparse=None,
-#           gene_labels='symbol',
-#           allow_duplicates=False,
-#           min_library_size=10,
-#           min_cells_per_gene=2)
+from phate_io import run_phate
 
-sys.stdout.write("OUT DATA TEST SUCESSFUL")
-sys.stdout.write("OUT DATA TEST SUCESSFUL 2")
-sys.stdout.write("OUT DATA TEST SUCESSFUL 3")
-sys.stdout.write("OUT DATA TEST SUCESSFUL 4")
-sys.stdout.write("OUT DATA TEST SUCESSFUL 5")
+run_phate("test_10X.zip",
+          sparse=None,
+          gene_labels='symbol',
+          allow_duplicates=False,
+          min_library_size=10,
+          min_cells_per_gene=2)
+
+sys.stdout.write("OUT DATA TEST SUCCESSFUL")
 
 exit(1)
